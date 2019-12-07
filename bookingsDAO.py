e7e6262438bc26da2a8186e6677d54b31cc58b2b
Patrick Moore <moorepa@outlook.com>
2019-12-07 08:31:51 +0000

    added config file

--- bookingsDAO.py
+++ bookingsDAO.py
@@ -7,7 +7,7 @@ class BookingsDAO:
   def __init__(self):
     self.db = mysql.connector.connect( 
     host=cfg.mysql['host'],
-    user=cfg.mysql['username'],
+    user=cfg.mysql['user'],
     password=cfg.mysql['password'],
     database=cfg.mysql['database']
     )
