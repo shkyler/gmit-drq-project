ec8a518db8507ba28654ffd348ad1de24a7b1112
Patrick Moore <moorepa@outlook.com>
2019-12-09 20:29:52 +0000

    added connection pooling

--- bookingsDAO.py
+++ bookingsDAO.py
@@ -3,118 +3,133 @@ import dbconfig as cfg
 
 
 class BookingsDAO:
-  db=""
-  def connectToDB(self):
-    self.db = mysql.connector.connect(
+  
+  def initConnectToDB(self):
+    db = mysql.connector.connect(
       host=       cfg.mysql['host'],
       user=       cfg.mysql['user'],
       password=   cfg.mysql['password'],
-      database=   cfg.mysql['database']
-      )
+      database=   cfg.mysql['database'],
+      pool_name='my_connection_pool',
+      pool_size=20
+    )
+    return db  
 
-  def __init__(self): 
-    self.connectToDB()
+  def getConnection(self):
+    db = mysql.connector.connect(
+      pool_name='my_connection_pool'
+    )
+    return db 
 
-  def getCursor(self):
-    if not self.db.is_connected():
-        self.connectToDB()
-    return self.db.cursor()  
+  def __init__(self):
+    db=self.initConnectToDB()
+    db.close()
 
   def getAllRooms(self):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="select * from rooms" 
     cursor.execute(sql)
     results = cursor.fetchall() 
     returnArray = []
     for result in results:
       returnArray.append(self.roomsDict(result))
-    cursor.close()
+    db.close()
     return returnArray
     
 
   def findRoomByID(self, id):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="select * from rooms where id = %s" 
     values = (id,)
     cursor.execute(sql, values) 
     result = cursor.fetchone() 
     room = self.roomsDict(result)
-    cursor.close()
+    db.close()
     return room
     
 
   def createRoom(self, values):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="insert into rooms (name, colour) values (%s,%s)" 
     cursor.execute(sql, values)
-    self.db.commit() 
+    db.commit() 
     lastRowID = cursor.lastrowid
-    cursor.close() 
+    db.close() 
     return lastRowID
      
 
   def updateRoom(self, values):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="update rooms set name= %s, colour=%s where id = %s" 
     cursor.execute(sql, values)
-    self.db.commit()
-    cursor.close() 
+    db.commit()
+    db.close() 
 
   def deleteRoom(self, id):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="delete from rooms where id = %s"
     values = (id,) 
     cursor.execute(sql, values)
-    self.db.commit()
-    cursor.close() 
+    db.commit()
+    db.close() 
 
   def getAllBookings(self):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="select * from bookings" 
     cursor.execute(sql)
     results = cursor.fetchall() 
     returnArray = []
     for result in results:
       returnArray.append(self.bookingsDict(result))
-    cursor.close()
+    db.close()
     return returnArray
     
 
   def findBookingsByID(self, id):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="select * from bookings where id = %s" 
     values = (id,)
     cursor.execute(sql, values) 
     result = cursor.fetchone() 
     booking = self.bookingsDict(result)
-    cursor.close()
+    db.close()
     return booking
     
 
   def createBooking(self, values):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="insert into bookings (roomID, dateRequired, userName, reason) values (%s,%s,%s,%s)" 
     cursor.execute(sql, values)
-    self.db.commit() 
+    db.commit() 
     lastRowID = cursor.lastrowid 
-    cursor.close()
+    db.close()
     return lastRowID 
     
 
   def updateBooking(self, values):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="update bookings set roomID= %s, dateRequired=%s, userName=%s, reason=%s where id = %s" 
     cursor.execute(sql, values)
-    self.db.commit()
-    cursor.close()
+    db.commit()
+    db.close()
 
   def deleteBooking(self, id):
-    cursor = self.getCursor()
+    db = self.getConnection()
+    cursor = db.cursor()
     sql="delete from bookings where id = %s"
     values = (id,) 
     cursor.execute(sql, values)
-    self.db.commit()  
-    cursor.close()
+    db.commit()  
+    db.close()
 
   # convert rooms result to a dictionary
   def roomsDict(self,result):
